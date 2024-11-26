ff41028ce60b65bdf8b8fe80afc56653c5fbdf8f
TheodoreUtomo <theodoreutomo@gmail.com>
2024-11-25 22:42:54 -0500

    Initial Commit

---
+++ MoveOS/MoveOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveOSApp.swift
+//  MoveOS
+//
+//  Created by Theodore Utomo on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MoveOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
